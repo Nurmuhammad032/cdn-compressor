25a8a8951d4ede017d09ec19a9698c9988b9cbf7
Nurmuhammad <nurmuhammad2003o@gmail.com>
2025-05-08 11:59:04 +0500

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App() {
+  const searchParams = useSearchParams();
+  const w = searchParams.get("w");
+  const h = searchParams.get("h");
+  const q = searchParams.get("q");
+  const src = searchParams.get("src");
+  const objectFit = searchParams.get("objectFit");
+
+  return (
+    <Image
+      src={src ? decodeURIComponent(src) : ""}
+      alt="Compressed image"
+      width={w ? parseInt(w) : 0}
+      height={h ? parseInt(h) : 0}
+      quality={q ? parseInt(q) : 0}
+      objectFit={objectFit ? (objectFit as any) : "cover"}
+    />
+  );
 }
