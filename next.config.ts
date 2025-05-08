997ac16844c02763eda98fa24d1ed7293e7085e7
Nurmuhammad <nurmuhammad2003o@gmail.com>
2025-05-08 12:05:31 +0500

    fix: fixed .env

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  images: {
-    remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_CDN_URL}`)],
-  },
+  // images: {
+  //   remotePatterns: [new URL(`${process.env.NEXT_PUBLIC_CDN_URL}`)],
+  // },
 };
 
 export default nextConfig;
